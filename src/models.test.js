67d195e7c987b14a27885d03c9c9911f86fd083c
Murilo Leonel <murilowsd@gmail.com>
2022-09-23 19:20:08 -0300

    parceiro

--- src/models.test.js
+++ src/models.test.js
@@ -83,3 +83,98 @@ describe("Cliente", () => {
 
     });
 });
+
+describe ("Parceiro", () =>{
+    test("List", async () => {
+        await Parceiro.bulkCreate([
+            {
+                nome: "Murilo",
+                cnpj: "123456789",
+                telefone: "123456789",
+                proprietario: "Leonel",
+                cpf:"123456789"
+            },
+            {
+                nome: "Mane",
+                cnpj: "234567891",
+                telefone: "234567891",
+                proprietario: "Jose",
+                cpf:"234567891"
+            },
+            {
+                nome: "Kaka",
+                cnpj: "345678912",
+                telefone: "345678912",
+                proprietario: "Lala",
+                cpf:"345678912"
+            },
+            {
+                nome: "Zezinho",
+                cnpj: "456789123",
+                telefone: "456789123",
+                proprietario: "Zzzz",
+                cpf:"456789123"
+            },
+
+        ])
+
+        const parceiro = await Parceiro.findAll();
+
+        expect(parceiro.length).toBe(4);
+
+    })
+    
+    test("Insert", async () => {
+        const parceiro = await Parceiro.create({
+                nome: "Baratinha",
+                cnpj: "567891234",
+                telefone: "567891234",
+                proprietario: "tinha",
+                cpf:"567891234"
+        });
+        expect(parceiro.id).toBeDefined();
+
+        // Inserindo com os dados brancos
+        await expect(Parceiro.create({
+            id: parceiro.id,
+            nome: "",
+            email: "",
+            password: ""
+        })).rejects.toThrow();
+
+        await expect(Parceiro.create({
+            id: parceiro.id,
+            nome: "",
+            email: "xxx",
+            password: "xxx",
+        })).rejects.toThrow();
+
+        await expect(Parceiro.create({
+            id: parceiro.id,
+            nome: "xxx",
+            email: "",
+            password: "xxx",
+        })).rejects.toThrow();
+
+        await expect(Parceiro.create({
+            id: parceiro.id,
+            nome: "xxx",
+            email: "xxx",
+            password: "",
+        })).rejects.toThrow();
+
+        // Inserindo sem nenhuma informação
+        await expect(Parceiro.create({})).rejects.toThrow();
+
+    
+
+        
+
+        
+
+    });
+
+
+
+
+})
